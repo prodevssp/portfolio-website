d2774edc6ae26faa1d64cb1b062599448c4a5399
Ayush Chugh <hey@ayushchugh.com>
2024-12-05 00:36:10 +0530

    feat: clerk auth

---
+++ src/app/signup/[[...signup]]/page.jsx
@@ -0,0 +1,14 @@
+import { SignUp } from "@clerk/nextjs";
+import React from "react";
+
+const SignupPage = () => {
+	return (
+		<div>
+			<div>
+				<SignUp />
+			</div>
+		</div>
+	);
+};
+
+export default SignupPage;
