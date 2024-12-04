8fd5d1ef882ed5797d6824bc84c8880ff228b75c
Ayush Chugh <hey@ayushchugh.com>
2024-12-05 00:53:42 +0530

    feat: restructure homepage with HeroSection component and update styles and constants file

---
+++ src/lib/constants.js
@@ -0,0 +1,6 @@
+export const basicDetails = {
+    name: 'James Smith',
+    role: 'Creative Designer',
+    location: 'New York',
+    about: `I'm a creative designer based in New York, and I'm very passionate and dedicated to my work.`,
+}
