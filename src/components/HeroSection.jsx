252a5787e5f546542c365b8dbe86e818482c39df
Ayush Chugh <hey@ayushchugh.com>
2024-12-05 23:34:24 +0530

    feat: hero section

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -1,50 +1,60 @@
-import { basicDetails } from '@/lib/constants';
-import Image from 'next/image';
+import config from "@/lib/config";
+import Image from "next/image";
+import Button from "./ui/Button";
+import MouseScrollAnimation from "./ui/MouseScrollAnimation";
+import { FaLinkedinIn, FaGithub } from "react-icons/fa";
+import { IoMdMail } from "react-icons/io";
 
 export default function HeroSection() {
 	return (
-		<section className='w-full h-screen flex items-center justify-center bg-gradient-to-br from-white via-[#f7f5f7] to-[#e4ddee]'>
-			<div className='flex flex-col items-start space-y-4'>
-				<h2 className='text-2xl text-orange-600'>
-					Hello, I&apos;m
-				</h2>
-				<h1 className='text-6xl font-bold text-black'>
-					{basicDetails.name}
-				</h1>
-				<p className='text-lg text-gray-500'>
-					A{' '}
-					<span className='text-green-600'>
-						{basicDetails.role}
-					</span>{' '}
-					From{' '}
-					<span className='text-blue-600'>
-						{basicDetails.location}
-					</span>
-				</p>
-				<p className='text-gray-500 max-w-md'>
-					{basicDetails.about}
-				</p>
-				<button className='px-6 py-2 mt-4 text-white bg-orange-500 rounded-full shadow hover:bg-orange-600'>
-					About Me
-				</button>
-			</div>
-
-			{/* <div className=' right-24 flex items-center space-x-4'>
-				<div className='p-2 bg-yellow-300 rounded-full'>
-					Ai
+		<section className="h-screen w-full flex items-center justify-center bg-[url('/assets/backgrounds/hero-bg.jpg')]">
+			<div className="max-w-7xl flex items-center justify-between">
+				<div className="flex flex-col items-start space-y-4">
+					<h2 className="text-2xl text-primary-500 font-semibold">
+						Hello, I&apos;m
+					</h2>
+					<h1 className="text-6xl font-medium text-black">
+						{config.basicDetails.name}
+					</h1>
+					<p className="text-lg text-gray-500">
+						A <span className="text-green-600">{config.basicDetails.role}</span>{" "}
+						From{" "}
+						<span className="text-blue-600">
+							{config.basicDetails.location}
+						</span>
+					</p>
+					<p className="text-gray-500 max-w-md">{config.basicDetails.about}</p>
+					<div className="flex gap-4 items-center mt-6">
+						<Button>About me</Button>
+						<div className="flex gap-2 items-center">
+							<div>
+								<a href={config.socials.linkedin} target="_blank">
+									<FaLinkedinIn size={20} />
+								</a>
+							</div>
+							<div>
+								<a href={config.socials.github} target="_blank">
+									<FaGithub size={20} />
+								</a>
+							</div>
+							<div>
+								<a href={config.socials.mail} target="_blank">
+									<IoMdMail size={20} />
+								</a>
+							</div>
+						</div>
+					</div>
 				</div>
-				<div className='p-2 bg-blue-600 text-white rounded-full'>
-					Ps
-				</div>
-				<div className='p-2 bg-white border border-gray-300 rounded-full'>
+				<div>
 					<Image
-						src='/figma-icon.png'
-						alt='Figma'
-						width={24}
-						height={24}
+						src={"/assets/avatar.svg"}
+						alt="avatar"
+						width={400}
+						height={400}
 					/>
 				</div>
-			</div> */}
+			</div>
+			<MouseScrollAnimation />
 		</section>
 	);
 }
---
+++ src/lib/config.js
@@ -0,0 +1,15 @@
+const config = {
+	basicDetails: {
+		name: "Soumya Sourav",
+		role: "Creative Designer",
+		location: "New York",
+		about: `I'm a creative designer based in New York, and I'm very passionate and dedicated to my work.`,
+	},
+	socials: {
+		linkedin: "https://www.linkedin.com/in/soumyasouravpatnaik/",
+		github: "https://github.com/sspcodeflix",
+		mail: "manager@soumyasourav.com",
+	},
+};
+
+export default config;
