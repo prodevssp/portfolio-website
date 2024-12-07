3f9730db9d386aaf6ebb9bead48cfc15cab23eee
Ayush Chugh <hey@ayushchugh.com>
2024-12-07 16:03:45 +0530

    feat: enhance HeroSection layout and styles for improved responsiveness

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -1,45 +1,97 @@
-import config from "@/lib/config";
-import Image from "next/image";
-import Button from "./ui/Button";
-import MouseScrollAnimation from "./ui/MouseScrollAnimation";
-import { FaLinkedinIn, FaGithub } from "react-icons/fa";
-import { IoMdMail } from "react-icons/io";
+import config from '@/lib/config';
+import Image from 'next/image';
+import Button from './ui/Button';
+import MouseScrollAnimation from './ui/MouseScrollAnimation';
+import { FaLinkedinIn, FaGithub } from 'react-icons/fa';
+import { IoMdMail } from 'react-icons/io';
 
 export default function HeroSection() {
 	return (
-		<section className="h-screen w-full flex items-center justify-center bg-[url('/assets/backgrounds/hero-bg.jpg')]">
-			<div className="max-w-7xl flex items-center justify-between">
-				<div className="flex flex-col items-start space-y-4">
-					<h2 className="text-2xl text-primary-500 font-semibold">
+		<section
+			className='h-screen flex items-center justify-center bg-no-repeat bg-cover bg-center'
+			style={{
+				backgroundImage:
+					"url('/assets/backgrounds/hero-bg.jpg')",
+			}}
+		>
+			<div className='max-w-6xl flex items-center justify-between w-full'>
+				<div className='flex flex-col items-start space-y-4'>
+					<h2 className='text-2xl text-primary-500 font-semibold'>
 						Hello, I&apos;m
 					</h2>
-					<h1 className="text-6xl font-medium text-black">
+					<h1 className='text-6xl font-medium text-black'>
 						{config.basicDetails.name}
 					</h1>
-					<p className="text-lg text-gray-500">
-						A <span className="text-green-600">{config.basicDetails.role}</span>{" "}
-						From{" "}
-						<span className="text-blue-600">
-							{config.basicDetails.location}
+					<p className='text-lg text-gray-500'>
+						A{' '}
+						<span className='text-green-600'>
+							{
+								config
+									.basicDetails
+									.role
+							}
+						</span>{' '}
+						From{' '}
+						<span className='text-blue-600'>
+							{
+								config
+									.basicDetails
+									.location
+							}
 						</span>
 					</p>
-					<p className="text-gray-500 max-w-md">{config.basicDetails.about}</p>
-					<div className="flex gap-4 items-center mt-6">
+					<p className='text-gray-500 max-w-md'>
+						{config.basicDetails.about}
+					</p>
+					<div className='flex gap-4 items-center mt-6'>
 						<Button>About me</Button>
-						<div className="flex gap-2 items-center">
+						<div className='flex gap-3 items-center'>
 							<div>
-								<a href={config.socials.linkedin} target="_blank">
-									<FaLinkedinIn size={20} />
+								<a
+									href={
+										config
+											.socials
+											.linkedin
+									}
+									target='_blank'
+								>
+									<FaLinkedinIn
+										size={
+											25
+										}
+									/>
 								</a>
 							</div>
 							<div>
-								<a href={config.socials.github} target="_blank">
-									<FaGithub size={20} />
+								<a
+									href={
+										config
+											.socials
+											.github
+									}
+									target='_blank'
+								>
+									<FaGithub
+										size={
+											25
+										}
+									/>
 								</a>
 							</div>
 							<div>
-								<a href={config.socials.mail} target="_blank">
-									<IoMdMail size={20} />
+								<a
+									href={
+										config
+											.socials
+											.mail
+									}
+									target='_blank'
+								>
+									<IoMdMail
+										size={
+											25
+										}
+									/>
 								</a>
 							</div>
 						</div>
@@ -47,8 +99,8 @@ export default function HeroSection() {
 				</div>
 				<div>
 					<Image
-						src={"/assets/avatar.svg"}
-						alt="avatar"
+						src={'/assets/avatar.svg'}
+						alt='avatar'
 						width={400}
 						height={400}
 					/>
