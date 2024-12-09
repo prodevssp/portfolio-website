e565e8e004a254a11ce122f96ebe1805d78b2177
Ayush Chugh <hey@ayushchugh.com>
2024-12-09 22:52:09 +0530

    feat: update styling for various components and improve accessibility with consistent color usage

--- src/components/ui/Button.jsx
+++ src/components/ui/Button.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Button = ({ children, sx }) => {
 	return (
 		<button
-			className={`px-6 py-3 text-white  bg-primary-500 rounded-lg shadow hover:bg-primary-600 transition font-semibold lg:text-lg xl:text-xl ${sx}`}
+			className={`px-6 py-3 text-slate-50  bg-primary-500 rounded-lg shadow hover:bg-primary-600 transition font-semibold lg:text-lg xl:text-xl ${sx}`}
 			type='submit'
 		>
 			{children}
