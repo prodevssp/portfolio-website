ac1df071674c566ada6f9ec4b3bc4d6099b157db
Ayush Chugh <hey@ayushchugh.com>
2024-12-12 18:40:30 +0530

    feat: dark theme in hero section and certificate layout

--- src/components/BlogSection.jsx
+++ src/components/BlogSection.jsx
@@ -1,82 +1,65 @@
-import React from 'react';
+import React from "react";
 
 const blogs = [
-	{
-		id: 1,
-		category: 'Web Development',
-		title: "Jim Morisson Says when the music's over turn off the light",
-		date: '23 Dec',
-		image: '/assets/project.png', // Replace with your actual image path
-	},
-	{
-		id: 2,
-		category: 'Branding',
-		title: 'How to be appreciated for your hard work as a developer',
-		date: '23 Dec',
-		image: '/assets/project.png', // Replace with your actual image path
-	},
-	{
-		id: 3,
-		category: 'Social Media',
-		title: 'How designers and developers can collaborate better',
-		date: '23 Dec',
-		image: '/assets/project.png', // Replace with your actual image path
-	},
+  {
+    id: 1,
+    category: "Web Development",
+    title: "Jim Morisson Says when the music's over turn off the light",
+    date: "23 Dec",
+    image: "/assets/project.png", // Replace with your actual image path
+  },
+  {
+    id: 2,
+    category: "Branding",
+    title: "How to be appreciated for your hard work as a developer",
+    date: "23 Dec",
+    image: "/assets/project.png", // Replace with your actual image path
+  },
+  {
+    id: 3,
+    category: "Social Media",
+    title: "How designers and developers can collaborate better",
+    date: "23 Dec",
+    image: "/assets/project.png", // Replace with your actual image path
+  },
 ];
 
 const BlogSection = () => {
-	return (
-		<section className='py-16 bg-slate-50 dark:bg-slate-900'>
-			<div className='max-w-6xl mx-auto text-center px-4'>
-				<h2 className='text-orange-500 text-lg font-semibold'>
-					From My Blog
-				</h2>
-				<h3 className='text-4xl font-bold mt-2 dark:text-slate-50 text-slate-800'>
-					Our Recent Updates, Blog, Tips, Tricks &
-					More
-				</h3>
-			</div>
+  return (
+    <section className="py-16 bg-slate-50 dark:bg-[#2C2D33]">
+      <div className="max-w-6xl mx-auto text-center px-4">
+        <h2 className="text-orange-500 text-lg font-semibold">From My Blog</h2>
+        <h3 className="text-4xl font-bold mt-2 dark:text-slate-50 text-slate-800">
+          Our Recent Updates, Blog, Tips, Tricks & More
+        </h3>
+      </div>
 
-			<div className='mt-12 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8 px-6 lg:px-12'>
-				{blogs.map(blog => (
-					<div
-						key={blog.id}
-						className='relative dark:bg-gray-800 hover:scale-105 bg-slate-50 rounded-lg shadow-lg overflow-hidden hover:shadow-xl transition-transform duration-300'
-					>
-						<img
-							src={blog.image}
-							alt={blog.title}
-							className='w-full h-56 object-cover'
-						/>
-						<div className='absolute top-4 left-4 bg-green-500 text-slate-50 text-center text-sm px-3 py-1 rounded-md shadow-md'>
-							<p>
-								{
-									blog.date.split(
-										' '
-									)[0]
-								}
-							</p>
-							<p>
-								{
-									blog.date.split(
-										' '
-									)[1]
-								}
-							</p>
-						</div>
-						<div className='p-6'>
-							<p className='text-orange-500 font-semibold'>
-								{blog.category}
-							</p>
-							<h4 className='text-xl font-bold dark:text-slate-50 text-gray-900 mt-2'>
-								{blog.title}
-							</h4>
-						</div>
-					</div>
-				))}
-			</div>
-		</section>
-	);
+      <div className="mt-12 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8 px-6 lg:px-12">
+        {blogs.map((blog) => (
+          <div
+            key={blog.id}
+            className="relative dark:bg-[#4b4f5c] hover:scale-105 bg-slate-50 rounded-lg shadow-lg overflow-hidden hover:shadow-xl transition-transform duration-300"
+          >
+            <img
+              src={blog.image}
+              alt={blog.title}
+              className="w-full h-56 object-cover"
+            />
+            <div className="absolute top-4 left-4 bg-green-500 text-slate-50 text-center text-sm px-3 py-1 rounded-md shadow-md">
+              <p>{blog.date.split(" ")[0]}</p>
+              <p>{blog.date.split(" ")[1]}</p>
+            </div>
+            <div className="p-6">
+              <p className="text-orange-500 font-semibold">{blog.category}</p>
+              <h4 className="text-xl font-bold dark:text-slate-50 text-gray-900 mt-2">
+                {blog.title}
+              </h4>
+            </div>
+          </div>
+        ))}
+      </div>
+    </section>
+  );
 };
 
 export default BlogSection;
--- src/components/CertificationSection.jsx
+++ src/components/CertificationSection.jsx
@@ -1,156 +1,80 @@
-'use client';
-import React, { useState, useEffect } from 'react';
-import { FaCircleChevronLeft, FaCircleChevronRight } from 'react-icons/fa6';
-import config from '@/lib/config';
+"use client";
+import React from "react";
+import Slider from "react-slick";
+import config from "@/lib/config";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 const CertificationSection = () => {
-	const [currentSlide, setCurrentSlide] = useState(0);
-	const [slidesPerView, setSlidesPerView] = useState(1);
+  const settings = {
+    dots: true,
+    infinite: true,
+    arrows: false,
+    speed: 500,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    responsive: [
+      {
+        breakpoint: 1024, // For medium screens
+        settings: {
+          slidesToShow: 2,
+        },
+      },
+      {
+        breakpoint: 768, // For small screens
+        settings: {
+          slidesToShow: 1,
+        },
+      },
+    ],
+    appendDots: (dots) => (
+      <div className="mt-8">
+        {/* styles are overwritten in globals.css   */}
+        <ul className="flex justify-center space-x-2">{dots}</ul>
+      </div>
+    ),
+  };
 
-	// Update slidesPerView based on window width
-	useEffect(() => {
-		const updateSlides = () => {
-			const width = window.innerWidth;
-			console.log('width', width);
-			if (width >= 1280) setSlidesPerView(4);
-			else if (width >= 1024) setSlidesPerView(3);
-			else if (width >= 768) setSlidesPerView(2);
-			else setSlidesPerView(1);
-		};
+  return (
+    <section className="py-16 pb-32  dark:bg-[#2C2D33] bg-slate-50 text-slate-50">
+      <div className="max-w-7xl mx-auto px-6 text-center">
+        <h2 className="text-4xl font-bold text-orange-500">Certification</h2>
+        <p className="text-2xl text-gray-400 mt-2">My Amazing Works</p>
+      </div>
 
-		updateSlides();
-		window.addEventListener('resize', updateSlides);
-		return () => window.removeEventListener('resize', updateSlides);
-	}, []);
-
-	const handleNext = () => {
-		setCurrentSlide(prev =>
-			Math.min(
-				prev + slidesPerView,
-				config.certificates.length - slidesPerView
-			)
-		);
-	};
-
-	const handlePrev = () => {
-		setCurrentSlide(prev => Math.max(prev - slidesPerView, 0));
-	};
-
-	return (
-		<section className='py-16 pb-32 dark:bg-gray-900 bg-slate-50 text-slate-50'>
-			<div className='max-w-7xl mx-auto px-6 text-center'>
-				<h2 className='text-4xl font-bold text-orange-500'>
-					Certification
-				</h2>
-				<p className='text-2xl text-gray-400 mt-2'>
-					My Amazing Works
-				</p>
-			</div>
-
-			<div className='mt-12 relative'>
-				<div className='flex items-center justify-center'>
-					{/* Previous Button */}
-					<button
-						onClick={handlePrev}
-						className='absolute left-4 z-10 bg-gray-700/50 rounded-full p-2'
-						disabled={currentSlide === 0}
-						aria-label='Previous Slide'
-					>
-						<FaCircleChevronLeft className='text-slate-50' />
-					</button>
-
-					{/* Carousel */}
-					<div className='w-full'>
-						<div
-							className='flex gap-4 transition-transform duration-300 ease-in-out  '
-							style={{
-								transform: `translateX(-${
-									(currentSlide *
-										100) /
-									slidesPerView
-								}%)`,
-								width: `${
-									(config
-										.certificates
-										.length -
-										1 *
-											100) /
-									slidesPerView
-								}%`,
-							}}
-						>
-							{config.certificates.map(
-								(
-									certificate,
-									index
-								) => (
-									<div
-										key={
-											index
-										}
-										className='w-full sm:w-1/2 md:w-1/3 lg:w-1/4 flex-shrink-0'
-									>
-										<div
-											className='dark:bg-gray-800   hover:scale-105 bg-slate-50 rounded-lg shadow-lg overflow-hidden hover:shadow-xl transition-transform duration-300
-										'
-										>
-											<img
-												src={
-													certificate.image
-												}
-												alt={
-													certificate.title
-												}
-												className='h-56 object-cover w-full'
-											/>
-											<div className='p-4'>
-												<h3 className='text-xl font-bold dark:text-slate-50 text-slate-800'>
-													{
-														certificate.title
-													}
-												</h3>
-												<p className='text-gray-400 text-sm mt-2'>
-													{
-														certificate.description
-													}
-												</p>
-												<a
-													href={
-														certificate.link
-													}
-													target='_blank'
-													rel='noopener noreferrer'
-													className='inline-block mt-4 px-4 py-2 text-sm font-semibold text-orange-500 border border-orange-500 rounded hover:bg-orange-500 hover:text-slate-50 transition'
-												>
-													View
-													Certificate
-												</a>
-											</div>
-										</div>
-									</div>
-								)
-							)}
-						</div>
-					</div>
-
-					{/* Next Button */}
-					<button
-						onClick={handleNext}
-						className='absolute right-4 z-10 bg-gray-700/50 rounded-full p-2'
-						disabled={
-							currentSlide >=
-							config.certificates
-								.length -
-								slidesPerView
-						}
-						aria-label='Next Slide'
-					>
-						<FaCircleChevronRight className='text-slate-50' />
-					</button>
-				</div>
-			</div>
-		</section>
-	);
+      <div className="mt-12 px-10">
+        <Slider {...settings}>
+          {config.certificates.map((certificate, index) => (
+            <div key={index} className="px-2 h-full mb-2">
+              <div className="h-full dark:bg-[#4B4F5C] hover:scale-105 bg-slate-50 rounded-lg shadow-lg overflow-hidden hover:shadow-xl transition-transform duration-300">
+                <img
+                  src={certificate.image}
+                  alt={certificate.title}
+                  className="h-56 object-cover w-full"
+                />
+                <div className="p-4">
+                  <h3 className="text-xl font-bold dark:text-slate-50 text-slate-800">
+                    {certificate.title}
+                  </h3>
+                  <p className="text-gray-400 text-sm mt-2">
+                    {certificate.description}
+                  </p>
+                  <a
+                    href={certificate.link}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="inline-block mt-4 px-4 py-2 text-sm font-semibold text-orange-500 border border-orange-500 rounded hover:bg-orange-500 hover:text-slate-50 transition"
+                  >
+                    View Certificate
+                  </a>
+                </div>
+              </div>
+            </div>
+          ))}
+        </Slider>
+      </div>
+    </section>
+  );
 };
 
 export default CertificationSection;
